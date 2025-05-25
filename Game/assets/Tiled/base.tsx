158719ddd5dcbac2c4bb766fd8656d6cd5c890f0
iojuedioe9rd <catsstudio81@gmail.com>
2025-05-25 21:50:06 +0100

    lol

--- Game/assets/Tiled/base.tsx
+++ Game/assets/Tiled/base.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="base" tilewidth="64" tileheight="64" tilecount="5" columns="0">
+<tileset version="1.10" tiledversion="1.11.2" name="base" tilewidth="64" tileheight="128" tilecount="7" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
   <image source="../textures/wall.png" width="64" height="64"/>
@@ -15,5 +15,11 @@
  </tile>
  <tile id="4">
   <image source="../textures/bad_cat_head.png" width="64" height="64"/>
+ </tile>
+ <tile id="5">
+  <image source="game_over_trigger_icon.png" width="64" height="64"/>
+ </tile>
+ <tile id="6">
+  <image source="../textures/door.png" width="64" height="128"/>
  </tile>
 </tileset>
